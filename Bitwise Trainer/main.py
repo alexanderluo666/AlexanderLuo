1163af5f2d0d3e7a68e2749b3e0e6acd641f8a5a
Alexander Luo <alexanderluo0910@gmail.com>
2026-04-04 14:24:10 +0800

    modified:   Bitwise Trainer/main.py

--- Bitwise Trainer/main.py
+++ Bitwise Trainer/main.py
@@ -5,12 +5,20 @@ def instructions():
     print("Welcome to Bitwise Trainer! \n")
     print("Here, we can learn about Bitwise calculations! \n")
     print("You can also save your progress and high score to challenge yourself! \n")
-    print("Hope to see you soon!")
-    print("The symbols are &, ^, |, <<, >>")
+    print("Hope to see you soon! \n")
+    print("The symbols are & and, ^ xor, | or, << left shift, >> right shift \n")
 
 def bitwise_trainer():
     while True:
         score = 0 
         ops = ['&', '|', '^']
+        generation1,generation2 = random.randint(0,255),random.randint(0,255)
+        operator = random.choice(ops)
+        
+        if operator == '&': real_ans = generation1 & generation2
+        elif operator == '|': real_ans = generation1 | generation2
+        else: real_ans = generation1 ^ generation2
+
 
 instructions()
+bitwise_trainer()
