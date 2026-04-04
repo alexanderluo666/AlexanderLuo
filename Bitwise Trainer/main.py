ec70aa9937b75dc33ad7fded614b344943cd9cc2
Alexander Luo <alexanderluo0910@gmail.com>
2026-04-04 13:53:59 +0800

    modified:   Bitwise Trainer/main.py

--- Bitwise Trainer/main.py
+++ Bitwise Trainer/main.py
@@ -6,6 +6,7 @@ def instructions():
     print("Here, we can learn about Bitwise calculations! \n")
     print("You can also save your progress and high score to challenge yourself! \n")
     print("Hope to see you soon!")
+    print("The symbols are &, ^, |, <<, >>")
 
 def bitwise_trainer():
     while True:
