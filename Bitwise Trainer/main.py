ebd4abbb334f343d440547901d74610ac0e0e643
Alexander Luo <alexanderluo0910@gmail.com>
2026-04-04 10:57:05 +0800

    new file:   Bitwise Trainer/main.py 	modified:   README.md

---
+++ Bitwise Trainer/main.py
@@ -0,0 +1,15 @@
+import random, time
+
+def instructions():
+    print("Hello there! \n")
+    print("Welcome to Bitwise Trainer! \n")
+    print("Here, we can learn about Bitwise calculations! \n")
+    print("You can also save your progress and high score to challenge yourself! \n")
+    print("Hope to see you soon!")
+
+def bitwise_trainer():
+    while True:
+        score = 0 
+        ops = ['&', '|', '^']
+
+instructions()
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # AlexanderLuo
 Debugged python 3.14 --> fixed pygame/pygame-ce
 Used 3.12 instead after trying to reinstall visual studios code + python 3.14
+
+Used Gemini for structure(not code) for Bitwise Trainer folder
