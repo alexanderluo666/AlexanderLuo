0a79ebb1cd71b3abf15942cd283cd0342bb91e59
Alexander Luo <alexanderluo0910@gmail.com>
2026-04-05 14:41:54 +0800

    modified:   Bitwise Trainer/main.py

--- Bitwise Trainer/main.py
+++ Bitwise Trainer/main.py
@@ -1,16 +1,42 @@
 import random, json ;from datetime import datetime #random for generation, datetime for records, and json for saving
-import json
 import os
 
 FILENAME = "scores.json"
 
-def instructions():
-    print("Hello there! \n")
+def instructions(start_name):
+    print(f"Hello {start_name}! \n")
     print("Welcome to Bitwise Trainer! \n")
     print("Here, we can learn about Bitwise calculations! \n")
     print("You can also save your progress and high score to challenge yourself! \n")
     print("The symbols are & and, ^ xor, | or, << left shift, >> right shift \n")
     print("If you want to quit, just type 'exit'! \n")
+
+DATA_FILE = "data.json"
+
+def add_new_name():
+
+    if not os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "w") as f:
+            json.dump({"player_names": [], "high_score": 0}, f)
+
+
+    with open(DATA_FILE, "r") as f:
+        data = json.load(f)
+
+
+    if not data["player_names"]: 
+        name = input("Welcome! Enter your primary display name: \n")
+        data["player_names"].append(name)
+        with open(DATA_FILE, "w") as f:
+            json.dump(data, f, indent=4)
+    
+    return data["player_names"]
+
+
+all_names = add_new_name()
+current_name = all_names[0]
+print(f"--- Logged in as: {current_name} --- \n")
+
 
 
 def save_score(new_score): #for saving progress using json library, FILENAME is defined at the front
@@ -34,12 +60,15 @@ def save_score(new_score): #for saving progress using json library, FILENAME is 
         data[0]["status"] = "ALL-TIME HIGH SCORE"
 
     data.sort(key=lambda x: x['score'], reverse=True) #sorting using lambda, reverse to get descending order
+    
 
+    
     with open(FILENAME, "w") as f:
         json.dump(data[:10], f, indent=4) #only best 10 are displayed to reduce memory usage
 
 def bitwise_trainer():
     score = 0
+    name = input("Please enter your name:\n")
     while True:
         ops = ['&', '|', '^']
         generation1,generation2 = random.randint(0,255),random.randint(0,255)
@@ -71,5 +100,6 @@ def bitwise_trainer():
             print(f"Final Score: {score}")
     save_score(score)
 
-instructions()
+
+instructions(current_name)
 bitwise_trainer()
